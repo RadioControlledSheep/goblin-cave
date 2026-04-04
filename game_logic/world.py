dd510cd5fa3c772724e3b4077e416115ace59c59
RadioControlledSheep <joeydorz@gmail.com>
2026-04-04 13:59:22 +0100

    Deciding on game logic

---
+++ game_logic/world.py
@@ -0,0 +1,7 @@
+# Generate the world, holding room, monsters, player, current room.
+def generate_world():
+    # populate rooms
+    # launch player generation
+    # set room to starting location
+    print("This is where main state is!")
+    return
--- main.py
+++ main.py
@@ -1,14 +1,13 @@
-import time
-
 from characters.generate_character import generate_character
-from game_logic.clearscreen import clearscreen
 from game_logic.game_logic import exit_game, show_start_screen
+from game_logic.world import generate_world
 
 
 def main():
     playing = True
     while playing:
         show_start_screen()
+        generate_world()
         player = generate_character()
         playing = False
 
