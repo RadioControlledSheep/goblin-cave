631ec9e930d01139da0782f40f56e915e4948b09
RadioControlledSheep <joeydorz@gmail.com>
2026-04-14 10:21:50 +0100

    Armour roll

--- characters/loot.py
+++ characters/loot.py
@@ -1,5 +1,13 @@
 import random
 
+from characters.items_and_spells import armour_set
 
-def generate_loot(items, weights, number_of_loots):
+
+def generate_loot(items, weights, number_of_loots=1):
     return random.choices(items, weights, k=number_of_loots)
+
+
+def armour_roll():
+    armour = armour_set()
+    weights = [10, 20, 20, 10, 5, 20, 15]
+    return generate_loot(armour, weights)
