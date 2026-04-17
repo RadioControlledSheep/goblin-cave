70e6db2f20ca6be4c7e1b7eca31abbbad78fbe01
RadioControlledSheep <joeydorz@gmail.com>
2026-04-17 19:20:12 +0100

    Started combat planning

---
+++ game_logic/combat.py
@@ -0,0 +1,6 @@
+#   roll iniative with agility modifier
+#   select attack from list
+#   roll to hit - agility/intelligemce possibly strength modifier vs agility
+#   roll damage - strength/intelligence possibly agility modifier
+#   adjust damage for attack type vs resistances/weaknesses
+#   deduct damage from health
