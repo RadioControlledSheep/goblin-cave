44821ba552baa22f55a3aed986806cb7be9b2b70
RadioControlledSheep <joeydorz@gmail.com>
2026-04-19 14:44:35 +0100

    laying combat foundations

--- game_logic/combat.py
+++ game_logic/combat.py
@@ -1,6 +1,19 @@
-#   roll iniative with agility modifier
-#   select attack from list
-#   roll to hit - agility/intelligemce possibly strength modifier vs agility
-#   roll damage - strength/intelligence possibly agility modifier
-#   adjust damage for attack type vs resistances/weaknesses
-#   deduct damage from health
+#   roll initiative with agility modifier
+#   for each mob/player:
+#       select attack from list
+#       select target
+#       roll to hit - agility/intelligemce possibly strength modifier vs agility
+#       roll damage - strength/intelligence possibly agility modifier
+#       adjust damage for attack type vs resistances/weaknesses
+#       deduct damage from health
+def combat(player, enemies):  # receive list of enemies and player
+    player_initiative = {player.name: roll_initiative(player)}
+    pass
+
+
+def roll_initiative(character):
+    pass
+
+
+def attack():  # should be in Character class
+    pass
